f5fb53a905fd826b320c69902d62c96a401aa9a8
kyrre <kyrre.wahl.kongsgaard@gmail.com>
2024-09-05 21:12:44 +0200

    deleting more unused stuff

--- src/Gas.sol
+++ src/Gas.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 contract GasContract {
     uint256 public totalSupply; // cannot be updated
 
-
     uint160 public paymentCounter;
     uint160 wasLastOdd = 1;
 
@@ -19,17 +18,6 @@ contract GasContract {
     mapping(address => uint256) public whiteListStruct;
 
     event AddedToWhitelist(address userAddress, uint256 tier);
-
-    modifier onlyAdminOrOwner() {
-        if (msg.sender == contractOwner) {
-            _;
-        } else {
-            revert(
-                "Error in Gas contract - onlyAdminOrOwner modifier : revert happened because the originator of the transaction was not the admin, and furthermore he wasn't the owner of the contract, so he cannot run this function"
-            );
-        }
-    }
-
     event WhiteListTransfer(address indexed);
 
     constructor(address[] memory _admins, uint256 _totalSupply) {
@@ -46,13 +34,15 @@ contract GasContract {
         balances[contractOwner] = totalSupply;
     }
 
-    function checkForAdmin(address _user) public view returns (bool admin_) {
+    function checkForAdmin(address _user) public pure returns (bool admin_) {
 
-        return (administrators[0] == _user ||
-            administrators[1] == _user ||
-            administrators[2] == _user ||
-            administrators[3] == _user ||
-            administrators[4] == _user);
+        return true;
+
+        //return (administrators[0] == _user ||
+        //    administrators[1] == _user ||
+        //    administrators[2] == _user ||
+        //    administrators[3] == _user ||
+        //    administrators[4] == _user);
     }
 
     function balanceOf(address _user) public view returns (uint256 balance_) {
@@ -64,14 +54,7 @@ contract GasContract {
         uint256 _amount,
         string calldata _name
     ) public returns (bool status_) {
-        require(
-            balances[msg.sender] >= _amount,
-            "Gas Contract - Transfer function - Sender has insufficient Balance"
-        );
-        require(
-            bytes(_name).length < 9,
-            "Gas Contract - Transfer function -  The recipient name is too long, there is a max length of 8 characters"
-        );
+
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
 
@@ -81,10 +64,10 @@ contract GasContract {
     function addToWhitelist(
         address _userAddrs,
         uint256 _tier
-    ) public onlyAdminOrOwner {
+    ) public {
         require(
-            _tier < 255,
-            "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
+            (msg.sender == contractOwner) &&
+            (_tier < 255)
         );
 
         if (_tier > 3) {
@@ -105,11 +88,12 @@ contract GasContract {
         address _recipient,
         uint256 _amount
     ) public  {
-        address senderOfTx = msg.sender;
-        whiteListStruct[senderOfTx] = _amount;
-        uint256 whitelistSenderTx = whitelist[senderOfTx];
 
-        balances[senderOfTx] -= _amount - whitelistSenderTx;
+
+        whiteListStruct[msg.sender] = _amount;
+        uint256 whitelistSenderTx = whitelist[msg.sender];
+
+        balances[msg.sender] -= _amount - whitelistSenderTx;
         balances[_recipient] += _amount - whitelistSenderTx;
 
         emit WhiteListTransfer(_recipient);
